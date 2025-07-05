6250941821e702b61edca729bfd971fa92025c3e
Matteo Iannuzzi <matteoiannuzzi03@gmail.com>
2025-07-05 11:25:07 +0200

    Delete Iannuzzi_html directory

--- Iannuzzi_html/Littel_Lemon_Matteo_Iannuzzi/Littel_Lemon.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  const navList = document.querySelector(".nav-list");
-  const navbar = document.querySelector(".navbar");
-
-  navList.classList.toggle("open");
-  navbar.classList.toggle("expanded");
-}
